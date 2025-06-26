5b8a71536ae05b530a07bcd1a81e27018000fb01
The Heretic's Court <me@breakerofhalos.monster>
2025-06-26 10:28:57 -0700

    Did too much between commits. Started building out niri as the WM, but I'm starting to see the point of the modularizations. I'll have to re-factor to make things more managable and modular.

---
+++ modules/nixos/core.nix
@@ -0,0 +1,74 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkDefault
+    mkForce
+    getExe
+    concatStringSep
+    ;
+in
+{
+  system.stateVersion = "24.11";
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = mkDefault "America/Los_Angeles";
+  hardware.bluetooth.enable = mkDefault true;
+  networking.networkmanager.enable = true;
+  users.mutableUsers = false;
+
+  systemd =
+  let
+    extraConfig = ''
+      DefaultTimeoutStartSec=15s
+      DefaultTimeoutStopSec=15s
+      DefaultTimeoutAbortSec=15s
+      DefaultDeviceTimeoutSec=15s
+    '';
+  in
+  {
+    inherit extraConfig;
+    user = { inherit extraConfig; };
+    services."getty@tty1".enable = false;
+    services."autovt@tty1".enable = false;
+    services."getty@tty7".enable = false;
+    services."autovt@tty7".enable = false;
+    services."kmsconvt@tty1".enable = false;
+    services."kmsconvt@tty7".enable = false;
+  };
+
+  environment.systemPackages = with pkgs; [
+    # CLI base tools
+    usbutils
+    pciutils
+    uutils-coreutils-noprefix
+    mako
+
+    # GUI
+    alacritty
+    zellij
+    tokyonight-gtk-theme
+  ];
+
+  programs = {
+    steam.enable = true;
+    direnv.enable = true;
+    dconf.enable = true;
+    seahorse.enable = true;
+  };
+
+  services = {
+    automatic-timezoned.enable = mkForce true;
+
+    geoclue2 = {
+      enable = mkDefault true;
+      geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+      appConfig.gammastep = {
+        isAllowed = true;
+        isSystem = true;
+      };
+    };
+  };
+}

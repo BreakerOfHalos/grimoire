c70e0d820b6d46540e6f0f9aa8e3d843feaa5c15
The Heretic's Court <me@breakerofhalos.monster>
2025-06-26 16:15:52 -0700

    Added grimoireLib instead of extending nixpkgs.lib so as not to confuse myself by forgetting what's mine.

---
+++ modules/nixos/lib.nix
@@ -0,0 +1,28 @@
+{ grimoireLib }:
+{ pkgs ? import <nixpkgs> {}, lib }:
+let
+  primaryMonitor = config: builtins.elemAt config.grimoire.device.monitors 0;
+
+  mkProgram =
+    pkgs: name: extraConfig:
+    lib.attrsets.recursiveUpdate {
+      enable = lib.options.mkEnableOption "Enable ${name}";
+      package = lib.options.mkPackageOption pkgs name { };
+  } extraConfig;
+
+  mkGraphicalService = lib.attrsets.recursiveUpdate {
+    Unit.PartOf = [ "graphical-session.target" ];
+    Unit.After = [ "graphical-session.target" ];
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
+
+  ifTheyExist = config: groups: lib.filter (group: lib.hasAttr group config.users.groups) groups;
+in
+{
+  inherit
+    primaryMonitor
+    mkProgram
+    mkGraphicalService
+    ifTheyExist
+    ;
+}   

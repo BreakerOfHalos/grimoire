aa50b9129ecc900506d5b73c6342cbd9bbf110c8
The Heretic's Court <me@breakerofhalos.monster>
2025-06-26 14:52:32 -0700

    Set up nixpkgs per viperML.

---
+++ modules/nixos/nixpkgs.nix
@@ -0,0 +1,19 @@
+let
+  sources = import ../../npins;
+  lib = import "${sources.nixpkgs}/lib";
+in
+import sources.nixpkgs {
+  config = {
+    allowUnfree = true;
+    allowUnfreePredicate = _: true;
+    allowVariants = false;
+    allowBroken = false;
+    permittedInsecurePackages = [ ];
+    allowUnsupportedSystems = true;
+    allowAliases = false;
+  };
+
+  overlays = [
+    (import ./overlay.nix lib)
+  ];
+}

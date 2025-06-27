d6e5c3d62e128c436a3417801ea2a322be00f704
The Heretic's Court <me@breakerofhalos.monster>
2025-06-27 00:32:07 -0700

    Added xdg-template to grimoireLib.

--- modules/nixos/lib.nix
+++ modules/nixos/lib.nix
@@ -24,6 +24,13 @@ let
     publicKey = "ssh-${key.type} ${key.key}";
     };
   };
+
+  XDG_CONFIG_HOME = "$HOME/.config";
+  XDG_CACHE_HOME = "$HOME/.cache";
+  XDG_DATA_HOME = "$HOME/.local/share";
+  XDG_STATE_HOME = "$HOME/.local/state";
+  XDG_BIN_HOME = "$HOME/.local/bin";
+  XDG_RUNTIME_DIR = "/run/user/$UID";
 in
 {
   inherit
@@ -32,4 +39,49 @@ in
     mkGraphicalService
     ifTheyExist
     ;
+
+    simple = {
+      dataHome = XDG_DATA_HOME;
+      configHome = XDG_CONFIG_HOME;
+      cacheHome = XDG_CACHE_HOME;
+    };
+
+  xdg-template = {
+    # global env
+    global = {
+      inherit
+        XDG_CONFIG_HOME
+        XDG_CACHE_HOME
+        XDG_DATA_HOME
+        XDG_STATE_HOME
+        XDG_BIN_HOME
+        XDG_RUNTIME_DIR
+        ;
+      PATH = [ "$bin" ];
+    };
+
+    user =
+      xdg:
+      let
+        data = xdg.dataHome;
+        config = xdg.configHome;
+        cache = xdg.cacheHome;
+      in
+      {
+        # desktop
+        XCOMPOSECACHE = "${cache}/X11/xcompose";
+        ERRFILE = "${cache}/X11/xsession-errors";
+        WINEPREFIX = "${data}/wine";
+
+        # programs
+        GNUPGHOME = "${data}/gnupg";
+        LESSHISTFILE = "${data}/less/history";
+        INPUTRC = "${config}/readline/inputrc";
+        MPLAYER_HOME = "${config}/mplayer";
+
+        # programming
+        IPYTHONDIR = "${config}/ipython";
+        JUPYTER_CONFIG_DIR = "${config}/jupyter";
+      };
+  };
 }   

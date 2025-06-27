8cdea3a41f1e0a5ff239db016ba8f2f5e325dcc1
The Heretic's Court <me@breakerofhalos.monster>
2025-06-27 00:45:37 -0700

    Populated defaults.nix so it can be evaluated.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,1 +1,17 @@
-
+{
+  imports = [
+    ./networking
+    ./system
+    ./wm
+    ./boot.nix
+    ./core.nix
+    ./hardware.nix
+    ./headless.nix
+    ./laptop.nix
+    ./lib.nix
+    ./nix.nix
+    ./nixpkgs.nix
+    ./profiles.nix
+    ./security.nix
+  ]
+}

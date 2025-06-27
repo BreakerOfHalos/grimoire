394543638c067f020647cc146e8a3efd16fdd3bc
The Heretic's Court <me@breakerofhalos.monster>
2025-06-26 20:45:19 -0700

    Added zram support to core.nix

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -44,4 +44,23 @@ in
   services.automatic-timezoned = {
     enable = lib.mkIf prof.graphical.enable;
   };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+
+    # defaults to 50
+    memoryPercent = 90;
+  };
+
+  boot.kernel.sysctl = lib.mkIf config.zramSwap.enable {
+
+ lib. 
+    # zram is relatively cheap, prefer swap
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    # zram is in memory, no need to readahead
+    "vm.page-cluster" = 0;
+  };
 }

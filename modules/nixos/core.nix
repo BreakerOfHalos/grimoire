b634b46927347351b13813f52674efa6e523ae62
The Heretic's Court <me@breakerofhalos.monster>
2025-06-26 20:41:47 -0700

    Set core options based on profiles configuration

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -3,51 +3,45 @@
   pkgs,
   ...
 }:
+let
+  prof = config.grimoire.profiles;
+in
 {
   system.stateVersion = "24.11";
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = mkDefault "America/Los_Angeles";
-  hardware.bluetooth.enable = lib.mkDefault true;
+  hardware.bluetooth.enable = lib.mkIf prof.graphical.enable;
   networking.networkmanager.enable = true;
   users.mutableUsers = false;
 
-  systemd =
-  let
-    extraConfig = ''
-      DefaultTimeoutStartSec=15s
-      DefaultTimeoutStopSec=15s
-      DefaultTimeoutAbortSec=15s
-      DefaultDeviceTimeoutSec=15s
-    '';
-  in
-  {
-    inherit extraConfig;
-    user = { inherit extraConfig; };
-    services."getty@tty1".enable = false;
-    services."autovt@tty1".enable = false;
-    services."getty@tty7".enable = false;
-    services."autovt@tty7".enable = false;
-    services."kmsconvt@tty1".enable = false;
-    services."kmsconvt@tty7".enable = false;
-  };
+  environment.systemPackages = with pkgs; lib.mkMerge [
 
-  environment.systemPackages = with pkgs; [
-    # CLI base tools
-    usbutils
-    pciutils
-    uutils-coreutils-noprefix
-    mako
+    (lib.mkIf prof.graphical.enable {
+      # CLI tools for workstations, not cluster machines
+      usbutils
+      pciutils
+      mako
 
-    # GUI
-    alacritty
-    zellij
-    tokyonight-gtk-theme
+      # GUI, obvi
+      alacritty
+      zellij
+      tokyonight-gtk-theme
+    })
+    
+    (# CLI base tools
+      uutils-coreutils-noprefix
+    )
   ];
 
   programs = {
-    steam.enable = true;
+    steam = {
+      enable = prof.gaming.enable;
+    };
+
     direnv.enable = true;
-    dconf.enable = true;
-    seahorse.enable = true;
+  };
+
+  services.automatic-timezoned = {
+    enable = lib.mkIf prof.graphical.enable;
   };
 }

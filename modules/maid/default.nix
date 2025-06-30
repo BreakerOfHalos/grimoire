26acea807aa4409d1c876d76bed26bf5545208ba
The Heretic's Court <me@breakerofhalos.monster>
2025-06-29 20:42:24 -0700

    Got defaults set with imports.

---
+++ modules/maid/default.nix
@@ -0,0 +1,13 @@
+{
+  imports = [
+    ./maid.nix
+    ./firefox
+    ./fish
+    ./helix
+    ./niri
+    ./quickshell
+    ./screenlocker
+    # ./wallpapers does not get imported because it's just assets
+    ./yazi
+  ];
+}

4c88f3d39a900009b74e6a80771038c0ae04c404
The Heretic's Court <me@breakerofhalos.monster>
2025-06-30 17:47:48 -0700

    Removed the redundant 'maid.nix' file and placed the config in default.nix.

--- modules/maid/default.nix
+++ modules/maid/default.nix
@@ -1,13 +1,46 @@
+{ lib
+, pkgs
+, config
+, ...
+}:
 {
   imports = [
     ./maid.nix
     ./firefox
     ./fish
     ./helix
+    ./mako
     ./niri
     ./quickshell
     ./screenlocker
     # ./wallpapers does not get imported because it's just assets
     ./yazi
   ];
+
+  maid = {
+    packages = builtins.attrValues {
+      inherit (pkgs)
+      starship
+      zoxide
+      eza
+      yazi
+      fzf
+      btop
+      hyfetch
+      helix
+      anyrun
+      obsidian
+      flameshot
+      obs-studio
+      morgen
+      orca-slicer
+      discordo
+      moonlight
+      nheko
+      junction
+      quickshell
+      ;
+    };
+  };
 }
+

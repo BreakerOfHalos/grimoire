12a66c2b4f721cca79fef0189d891469570fb794
The Heretic's Court <me@breakerofhalos.monster>
2025-06-30 19:22:24 -0700

    fixed up the rebuild script a bit, but I'm having issues with the NIX_PATH

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -26,11 +26,11 @@ let
 
   overlayAdditionalSources = final: prev: {
     # Switching lix over to the direct build
-    lix =
-      let
-        lixModule = import "${sources.nixosModule}/module.nix";
-        lixSrc = import sources.lix;
-      in final { imports = [ lixModule { lix = lixSrc; } ]; };
+    # lix =
+    #   let
+    #     lixModule = import "${sources.nixosModule}/module.nix";
+    #     lixSrc = import sources.lix;
+    #   in final { imports = [ lixModule { lix = lixSrc; } ]; };
 
     maid = (import sources.nix-maid) final ../modules/maid;
 

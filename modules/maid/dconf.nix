ee09bad593c6cfea110b4cc8eb37c3205ab6db80
The Heretic's Court <me@breakerofhalos.monster>
2025-06-30 18:03:16 -0700

    Added some dconf settings. TODO: Add font settings.

---
+++ modules/maid/dconf.nix
@@ -0,0 +1,9 @@
+{
+  dconf.settings + {
+    "/org/gnome/desktop/interface/color-scheme" = "prefer-dark";
+    "/org/gnome/desktop/interace/cursor-size" = 24;
+    "/org/gnome/desktop/interface/cursor-theme" = "Nordzy-catppuccin-sky";
+    "/org/gnome/desktop/interface/gtk-theme" = "Tokyonight-Dark";
+    "/org/gnome/desktop/interface/icon-theme" = "Tokyonight-Dark";
+  };
+}

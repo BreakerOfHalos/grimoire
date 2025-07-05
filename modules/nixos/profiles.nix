d2f4fefc510920524627cdc12f4afc8a47d3fbbb
The Heretic's Court <me@breakerofhalos.monster>
2025-07-05 10:33:43 -0700

    Moved the profiles into a dedicated folder for organizational clarity.

--- modules/nixos/profiles.nix
+++
@@ -1,9 +0,0 @@
-{lib, ...}:
-{
-  options.grimoire.profiles = {
-    graphical.enable = lib.mkEnableOption "Graphical Interface";
-    headless.enable = lib.mkEnableOption "Headless";
-    gaming.enable = lib.mkEnableOption "Gaming";
-    laptop.enable = lib.mkEnableOption "Laptop";
-  };
-}

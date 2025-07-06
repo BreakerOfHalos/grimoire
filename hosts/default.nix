e40d62891655b6f79e351223581b696a59dfe4f7
The Heretic's Court <me@breakerofhalos.monster>
2025-07-06 13:06:26 -0700

    Accidentally adopted a new top level attribute unintentionally. Tried simplifying the hosts/default.nix to just generic imports to avoid boilerplate.

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,15 +3,14 @@ let
   pkgs = import ../packages;
 in
 (
-  { modules, config }:
-  import "${sources.nixpkgs}/nixos/lib/eval-config.nix" {
-    system = null;
-    modules = [
+  { config, ... }:
+  {
+    imports = [
       {
         config.nixpkgs.pkgs = pkgs;
       }
       ../modules/nixos
       (import sources.nix-maid).nixosModules.default
-    ] ++ modules;
+    ];
   }
 )

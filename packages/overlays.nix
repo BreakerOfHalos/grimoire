58d902ca3b55edf25bf6d5f8740065bc32a969f0
breakerofhalos <me@breakerofhalos.monster>
2025-07-13 21:33:43 -0700

    Trying to remove the need for pipe operators.

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -11,18 +11,27 @@ let
     
   };
 
-  overlayAuto =
-    final: prev:
-    (
-      readDir ./.
-      |> lib.filterAttrs (_: value: value == "directory")
-      |> mapAttrs (
-        name: _:
-        final.callPackage ./${name} {
+  # overlayAuto =
+  #   final: prev:
+  #   (
+  #     readDir ./.
+  #     |> lib.filterAttrs (_: value: value == "directory")
+  #     |> mapAttrs (
+  #       name: _:
+  #       final.callPackage ./${name} {
           
-        }
-      )
-    ) // { };
+  #       }
+  #     )
+  #   ) // { };
+
+  overlayAuto = final: prev: (
+    lib.mapAttrs
+    (
+      name: _: final.callpackage ./${name} {
+        lib.filterAttrs (_: value: value == "directory") lib.readDir ./.
+      }
+    )
+  ) // { };
 
   overlayAdditionalSources = final: prev: {
     # Switching lix over to the direct build

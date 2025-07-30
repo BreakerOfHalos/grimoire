226e41a5d01ba90b0c8cde7d3ed820b2e910224d
BreakerOfHalos <me@breakerofhalos.monster>
2025-07-30 01:26:56 -0700

    Finished refactoring the system/boot and moved some stuff around.

--- system/boot/generic.nix
+++ system/boot/generic.nix
@@ -3,184 +3,114 @@
 , config
 , ...
 }:
-let
-  cfg = config.grimoire.system.boot;
-in
-{
-  options.grimoire.system.boot = {
-    enableKernelTweaks = lib.mkEnableOption "Security and performance related kernel parameters";
-    recommendedLoaderConfig = lib.mkEnableOption "Tweaks for common bootloader configs per my liking";
-    loadRecommendedModules = lib.mkEnableOption "Kernel modules that accomodate most use cases.";
 
-    kernel = lib.mkOption {
-      type = lib.type.raw;
-      default = pkgs.linuxPackages_latest;
-      description = "The kernel to use for the system.";
+{
+  boot = {
+    consoleLogLevel = 3;
+
+    # we set the kernel to be defaulted to the one set by our settings
+    # we happen to default this to the latest kernel sooo:
+    # always use the latest kernel, love the unstablity
+    kernelPackages = lib.mkOverride 500 cfg.kernel;
+
+    # whether to enable support for Linux MD RAID arrays
+    # as of 23.11>, this throws a warning if neither MAILADDR nor PROGRAM are set
+    swraid.enable = mkDefault false;
+
+    # increase the map count, this is important for applications that require a lot of memory mappings
+    # such as games and emulators
+    kernel.sysctl."vm.max_map_count" = 2147483642;
+
+    tmp = {
+      # we have to clean /tmp
+      cleanOnBoot = true;
     };
 
     initrd = {
-      enableTweaks = lib.mkEnableOption "Quality of life tweaks for the initrd stage.";
-      optimizeCompressor = lib.mkEnableOption ''
-        initrd compression algorithm for size.
-        Enabling this option will force initrd to use zstd (default) with
-        level 19 and -T0 (STDIN). This will reduce the initrd size greatly
-        at the cost of compression speed.
-        Not recommended for low-end hardware.
-      '';
-    };
+      verbose = false;
 
-    silentBoot = lib.mkEnableOption ''
-      Almost entirely silent boot process through the `quiet` kernel parameter.
-    '';
+      systemd.enable = true;
 
-    extraKernelParams = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [ ];
-      description = ''
-        Extra kernel parameters to be passed to the kernel.
-        This is useful for passing additional parameters to the kernel
-        that are not covered by the default parameters.
-      ''
-    };
-
-    extraModulePackages = lib.mkOption {
-      type = lib.types.listOf lib.types.package;
-      default = [ ];
-      example = lib.options.literalExpression ''With config.boot.kernelPackages; [acpi_call]'';
-      description = "Extra kernel modules to be loaded.";
-    };
-  };
-
-  config = {
-    boot = {
-      consoleLogLevel = 3;
-
-      # we set the kernel to be defaulted to the one set by our settings
-      # we happen to default this to the latest kernel sooo:
-      # always use the latest kernel, love the unstablity
-      kernelPackages = lib.mkOverride 500 cfg.kernel;
-
-      extraModulePackages = lib.mkDefault cfg.extraModulePackages;
-
-      # whether to enable support for Linux MD RAID arrays
-      # as of 23.11>, this throws a warning if neither MAILADDR nor PROGRAM are set
-      swraid.enable = mkDefault false;
-
-      # shared config between bootloaders
-      # they are set unless system.boot.loader != none
-      loader = {
-        # if set to 0, space needs to be held to get the boot menu to appear
-        timeout = mkForce 2;
-
-        # copy boot files to /boot so that /nix/store is not required to boot
-        # it takes up more space but it makes my messups a bit safer
-        generationsDir.copyKernels = true;
-
-        # we need to allow installation to modify EFI variables
-        efi.canTouchEfiVariables = true;
-      };
-
-      # increase the map count, this is important for applications that require a lot of memory mappings
-      # such as games and emulators
-      kernel.sysctl."vm.max_map_count" = 2147483642;
-
-      tmp = {
-        # we have to clean /tmp
-        cleanOnBoot = true;
-      };
-
-      initrd = lib.mkMerge [
-        (lib.mkIf cfg.initrd.enableTweaks {
-          verbose = false;
-
-          systemd.enable = true;
-
-          kernelModules = [
-            "nvme"
-            "xhci_pci"
-            "ahci"
-            "btrfs"
-            "sd_mod"
-            "dm_mod"
-            "bluetooth"
-            "btusb"
-            "uvcvideo"
-          ];
-
-          availableKernelModules = [
-            "vmd"
-            "usbhid"
-            "sd_mod"
-            "sr_mod"
-            "dm_mod"
-            "uas"
-            "usb_storage"
-            "rtsx_usb_sdmmc"
-            "rtsx_pci_sdmmc" # Realtek SD card interface (btw i hate realtek)
-            "ata_piix"
-            "virtio_pci"
-            "virtio_scsi"
-            "ehci_pci"
-          ];
-        })
-
-        (lib.mkIf cfg.initrd.optimizeCompressor {
-          compressor = "zstd";
-          compressorArgs = [
-            "-19"
-            "-T0"
-          ];
-        })
+      kernelModules = [
+        "nvme"
+        "xhci_pci"
+        "ahci"
+        "btrfs"
+        "sd_mod"
+        "dm_mod"
+        "bluetooth"
+        "btusb"
+        "uvcvideo"
       ];
 
-      kernelParams = 
-        lib.lists.optionals cfg.enableKernelTweaks [
-          # https://en.wikipedia.org/wiki/Kernel_page-table_isolation
-          # `auto` means kernel will automatically decide the pti state
-          "pti=auto" # on || off
+      availableKernelModules = [
+        "vmd"
+        "usbhid"
+        "sd_mod"
+        "sr_mod"
+        "dm_mod"
+        "uas"
+        "usb_storage"
+        "rtsx_usb_sdmmc"
+        "rtsx_pci_sdmmc" # Realtek SD card interface (btw i hate realtek)
+        "ata_piix"
+        "virtio_pci"
+        "virtio_scsi"
+        "ehci_pci"
+      ];
 
-          # Disable the intel_idle (it stinks anyway) driver and use acpi_idle instead
-          "idle=nomwait"
-
-          # Enable IOMMU for devices used in passthrough and provide better host performance
-          "iommu=pt"
-
-          # Disable usb autosuspend
-          "usbcore.autosuspend=-1"
-
-          # Disables resume and restores original swap space
-          "noresume"
-
-          # allow systemd to set and save the backlight state
-          "acpi_backlight=native"
-
-          # prevent the kernel from blanking plymouth out of the fb
-          "fbcon=nodefer"
-
-          # disable boot logo
-          "logo.nologo"
-
-          # disable the cursor in vt to get a black screen during intermissions
-          "vt.global_cursor_default=0"
-        ]
-        ++ lib.lists.optionals cfg.silentBoot [
-          # tell the kernel to not be verbose, the voices are too loud
-          "quiet"
-
-          # kernel log message level
-          "loglevel=3" # 1: system is unusable | 3: error condition | 7: very verbose
-
-          # udev log message level
-          "udev.log_level=3"
-
-          # lower the udev log level to show only errors or worse
-          "rd.udev.log_level=3"
-
-          # disable systemd status messages
-          # rd prefix means systemd-udev will be used instead of initrd
-          "systemd.show_status=auto"
-          "rd.systemd.show_status=auto"
-        ]
+      compressor = "zstd";
+        compressorArgs = [
+          "-19"
+          "-T0"
+        ];
     };
+
+    kernelParams = [
+      # https://en.wikipedia.org/wiki/Kernel_page-table_isolation
+      # `auto` means kernel will automatically decide the pti state
+      "pti=auto" # on || off
+
+      # Disable the intel_idle (it stinks anyway) driver and use acpi_idle instead
+      "idle=nomwait"
+
+      # Enable IOMMU for devices used in passthrough and provide better host performance
+      "iommu=pt"
+
+      # Disable usb autosuspend
+      "usbcore.autosuspend=-1"
+
+      # Disables resume and restores original swap space
+      "noresume"
+
+      # allow systemd to set and save the backlight state
+      "acpi_backlight=native"
+
+      # prevent the kernel from blanking plymouth out of the fb
+      "fbcon=nodefer"
+
+      # disable boot logo
+      "logo.nologo"
+
+      # disable the cursor in vt to get a black screen during intermissions
+      "vt.global_cursor_default=0"
+
+      # tell the kernel to not be verbose, the voices are too loud
+      "quiet"
+
+      # kernel log message level
+      "loglevel=3" # 1: system is unusable | 3: error condition | 7: very verbose
+
+      # udev log message level
+      "udev.log_level=3"
+
+      # lower the udev log level to show only errors or worse
+      "rd.udev.log_level=3"
+
+      # disable systemd status messages
+      # rd prefix means systemd-udev will be used instead of initrd
+      "systemd.show_status=auto"
+      "rd.systemd.show_status=auto"
+    ];
   };
 }                             
--- system/boot/loader.nix
+++ system/boot/loader.nix
@@ -4,10 +4,22 @@
 , ... 
 }:
 {
-  boot.loader.systemd-boot = {
-    enable = lib.mkDefault true;
-    configurationLimit = 15;
-    consoleMode = lib.mkDefault "max";
-    editor = false;
+  boot.loader = {
+    systemd-boot = {
+      enable = lib.mkDefault true;
+      configurationLimit = 15;
+      consoleMode = lib.mkDefault "max";
+      editor = false;
+    };
+
+    # if set to 0, space needs to be held to get the boot menu to appear
+    timeout = mkForce 2;
+
+    # copy boot files to /boot so that /nix/store is not required to boot
+    # it takes up more space but it makes my messups a bit safer
+    generationsDir.copyKernels = true;
+
+    # we need to allow installation to modify EFI variables
+    efi.canTouchEfiVariables = true;
   };
 }

90f23e34aabf0acf0340ee95cce67f02ac6d354f
breakerofhalos <me@breakerofhalos.monster>
2025-10-11 22:20:45 -0700

    Trying to sort out Steam launching weirdness

--- system/wayland.nix
+++ system/wayland.nix
@@ -1,7 +1,26 @@
 { ... }:
 {
-  environment.variables = {
-    NIXOS_OZONE_WL = "1";
-    MOZ_ENABLE_WAYLAND = "1";
+  environment = {
+    variables = {
+      NIXOS_OZONE_WL = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+    };
+
+    sessionVariables = {
+      NIXOS_OZONE_WL = 1;
+      XDG_CURRENT_DESKTOP = "niri";
+      XDG_SESSION_TYPE = "wayland";
+      XDG_SESSION_DESKTOP = "niri";
+
+      SDL_VIDEODRIVER = "wayland";
+
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+
+      CLUTTER_BACKEND = "wayland";
+
+      GDK_BACKEND = "wayland";
+
+      QT_QPA_PLATFORM = "wayland";
+    };
   };
 }
